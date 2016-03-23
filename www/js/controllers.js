0deadf2192dd25988c314a72b01cc7666a51f6df
Bekzot Azimov <bazimov@gogoair.com>
2016-03-23 15:44:39 -0500

    change urls

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,42 +5,9 @@ angular.module('starter.controllers', ['ionic', 'ionic-audio'])
 
 .controller('ChatsCtrl',['$scope', '$http', function($scope , $http) {
 
-	$http.get('http://api.azimov.xyz/api/v1.0/albums/Abdulloh/').success( function(response){
+	$http.get('http://api.azimov.xyz/api/ios/albums/Abdulloh/').success( function(response){
 
-		//$scope.musics = response.albums;
-		$scope.musics = [
-    {
-      "album": "Abu_Hanifa",
-      "category": "Abdulloh",
-      "id": 5,
-      "items": [
-				{
-        "name": "01_Abu_Hanifa.mp3",
-				"url": "http://dfh59cyusxnu7.cloudfront.net/Abdulloh/Abu_Hanifa/01_Abu_Hanifa.mp3"
-			},
-        {
-				"name":	"02_Abu_Hanifa.mp3",
-				"url": "http://dfh59cyusxnu7.cloudfront.net/Abdulloh/Abu_Hanifa/01_Abu_Hanifa.mp3"
-				}
-      ]
-    },
-    {
-      "album": "Ali",
-      "category": "Abdulloh",
-      "id": 17,
-      "items": [
-				{
-        "name": "01_Abu_Hanifa.mp3",
-				"url": "http://dfh59cyusxnu7.cloudfront.net/Abdulloh/Abu_Hanifa/01_Abu_Hanifa.mp3"
-			},
-        {
-				"name":	"02_Abu_Hanifa.mp3",
-				"url": "http://dfh59cyusxnu7.cloudfront.net/Abdulloh/Abu_Hanifa/01_Abu_Hanifa.mp3"
-				}
-
-      ]
-    }
-	];
+		$scope.musics = response.albums;
 
 	});
 
@@ -49,43 +16,10 @@ angular.module('starter.controllers', ['ionic', 'ionic-audio'])
 
 .controller('ChatDetailCtrl', function($scope, $http, $stateParams, Chats, $ionicModal, $cordovaMedia) {
 $scope.musicId=$stateParams.chatId;
- $http.get('http://api.azimov.xyz/api/v1.0/albums/Abdulloh/').success( function(response){
+ $http.get('http://api.azimov.xyz/api/ios/albums/Abdulloh/').success( function(response){
 
-		//$scope.musics = response.albums;
-		$scope.musics = [
-    {
-      "album": "Abu_Hanifa",
-      "category": "Abdulloh",
-      "id": 5,
-      "items": [
-				{
-        "name": "01_Abu_Hanifa.mp3",
-				"url": "http://dfh59cyusxnu7.cloudfront.net/Abdulloh/Abu_Hanifa/01_Abu_Hanifa.mp3"
-			},
-        {
-				"name":	"02_Abu_Hanifa.mp3",
-				"url": "http://dfh59cyusxnu7.cloudfront.net/Abdulloh/Abu_Hanifa/01_Abu_Hanifa.mp3"
-				}
-      ]
-    },
-    {
-      "album": "Ali",
-      "category": "Abdulloh",
-      "id": 17,
-      "items": [
-				{
-        "name": "01_Abu_Hanifa.mp3",
-				"url": "http://dfh59cyusxnu7.cloudfront.net/Abdulloh/Abu_Hanifa/01_Abu_Hanifa.mp3"
-			},
-        {
-				"name":	"02_Abu_Hanifa.mp3",
-				"url": "http://dfh59cyusxnu7.cloudfront.net/Abdulloh/Abu_Hanifa/01_Abu_Hanifa.mp3"
-				}
+		$scope.musics = response.albums;
 
-      ]
-    }
-	];
-	
 	});
 
 
